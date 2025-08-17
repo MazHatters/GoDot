920eb5d6a4ca9653028ffc900c5482b1c263c8bd
Taufiq Wong <tawong@u91z03s03.42kl.edu.my>
2025-08-17 21:14:10 +0800

    Added duck.png in self_made folder, created .gitignore to ignore wine appimage for iMac

--- bored/scripts/main_menu.gd
+++ bored/scripts/main_menu.gd
@@ -1,9 +1,10 @@
 extends Control
 
+@export var scene : String
 
 # Handling Play button
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene/main.tscn")
+	get_tree().change_scene_to_file(scene)
 
 # Handling Quit button
 func _on_quit_pressed() -> void:
