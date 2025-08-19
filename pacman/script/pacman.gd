dc6196ffd4dafdb7aa9ae8f4a2f1df9c7e82fbd3
Taufiq Wong <tawong@u81z04s02.42kl.edu.my>
2025-08-19 19:18:54 +0800

    Modified movement script for pacman, need to create up/down sprite, probably just gonna use flip_v if possible

--- pacman/script/pacman.gd
+++ pacman/script/pacman.gd
@@ -2,44 +2,35 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
+var moving = false # To show which input gets priority
 #const JUMP_VELOCITY = -400.0
 @onready var sprite= $AnimatedSprite2D
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Handles left and right
+func _physics_process(delta):
+	# Get inputs
 	var dirx := Input.get_axis("ui_left", "ui_right")
-	if dirx:
-		if dirx < 0:
-			sprite.flip_h = true
-			sprite.play("moving")
-		if dirx > 0:
-			sprite.flip_h = false
-			sprite.play("moving")
-		velocity.x = dirx * SPEED
-	else:
-		sprite.play("idle")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	# Handles up and down
 	var diry := Input.get_axis("ui_up", "ui_down")
-	if diry:
-		if diry < 0:
-			#sprite.rotation = diry
-			sprite.play("moving")
-		if diry > 0:
-			#sprite.rotation = diry
-			sprite.play("moving")
+
+	# Apply movement
+	if dirx != 0:
+		velocity.x = dirx * SPEED
+		sprite.flip_h = dirx < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0 , SPEED)
+
+	if diry != 0:
 		velocity.y = diry * SPEED
 	else:
-		sprite.play("idle")
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	# Determine if moving
+	moving = dirx != 0 or diry != 0
+
+	# Play animation
+	if moving:
+		sprite.play("moving")
+	else:
+		sprite.play("idle")
+
 	move_and_slide()
