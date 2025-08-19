a3388b51a8a33767aada5ad10845ca5077068ac6
MazHatters <scp7074@gmail.com>
2025-08-20 01:01:50 +0800

    Modified script for pacman, added more animations for pacman

--- pacman/script/pacman.gd
+++ pacman/script/pacman.gd
@@ -1,36 +1,40 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
-var moving = false # To show which input gets priority
-#const JUMP_VELOCITY = -400.0
-@onready var sprite= $AnimatedSprite2D
-
+var moving = false
+var last_dir = Vector2.RIGHT  # default facing right
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Get inputs
 	var dirx := Input.get_axis("ui_left", "ui_right")
 	var diry := Input.get_axis("ui_up", "ui_down")
 
-	# Apply movement
+	velocity = Vector2.ZERO
+	moving = false
+
+	# Horizontal movement
 	if dirx != 0:
 		velocity.x = dirx * SPEED
+		sprite.play("moving_left_right")
 		sprite.flip_h = dirx < 0
-	else:
-		velocity.x = move_toward(velocity.x, 0 , SPEED)
+		last_dir = Vector2(dirx, 0)
+		moving = true
 
-	if diry != 0:
+	# Vertical movement
+	elif diry != 0:
 		velocity.y = diry * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		sprite.play("moving_up_down")
+		sprite.flip_v = diry > 0
+		last_dir = Vector2(0, diry)
+		moving = true
 
-	# Determine if moving
-	moving = dirx != 0 or diry != 0
-
-	# Play animation
-	if moving:
-		sprite.play("moving")
-	else:
-		sprite.play("idle")
+	# Idle animation depends on last direction
+	if !moving:
+		if last_dir.x != 0:
+			sprite.play("idle_left_right")
+			sprite.flip_h = last_dir.x < 0
+		elif last_dir.y != 0:
+			sprite.play("idle_up_down")
+			sprite.flip_v = last_dir.y > 0
 
 	move_and_slide()
